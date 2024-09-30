bdf7dd6dffae725dfb7dfa4cbd253fabcee885a3
Rizvi Fazi <rizvifazi124@gmail.com>
2024-09-30 23:17:10 +0530

    First commit, with all code in a single file

---
+++ sbur-rest-demo/src/main/java/com/thehecklers/sbur_rest_demo/SburRestDemoApplication.java
@@ -0,0 +1,114 @@
+package com.thehecklers.sbur_rest_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class SburRestDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SburRestDemoApplication.class, args);
+	}
+
+}
+
+class Coffee {
+
+	private final String id;
+	private String name;
+
+	public Coffee(String id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+
+	public Coffee(String name) {
+		this(UUID.randomUUID().toString(), name);
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
+
+@RestController
+@RequestMapping("/coffees")
+class RestApiDemoController {
+	private List<Coffee> coffees = new ArrayList<>();
+
+	public RestApiDemoController() {
+		coffees.addAll(List.of(
+				new Coffee("Café Cereza"),
+				new Coffee("Café Ganador"),
+				new Coffee("Café Lareño"),
+				new Coffee("Café Três Pontas")));
+	}
+
+	@GetMapping
+	Iterable<Coffee> getCoffees() {
+		return coffees;
+	}
+
+	@GetMapping("/{id}")
+	Optional<Coffee> getCoffeeById(@PathVariable String id) {
+		for (Coffee c : coffees) {
+			if (c.getId().equals(id)) {
+				return Optional.of(c);
+			}
+		}
+
+		return Optional.empty();
+	}
+
+	@PostMapping
+	Coffee postCoffee(@RequestBody Coffee coffee) {
+		coffees.add(coffee);
+		return coffee;
+	}
+
+	@PutMapping("/{id}")
+	ResponseEntity<Coffee> putCoffee(@PathVariable String id,
+			@RequestBody Coffee coffee) {
+		int coffeeIndex = -1;
+
+		for (Coffee c : coffees) {
+			if (c.getId().equals(id)) {
+				coffeeIndex = coffees.indexOf(c);
+				coffees.set(coffeeIndex, coffee);
+			}
+		}
+
+		return (coffeeIndex == -1) ? new ResponseEntity<>(postCoffee(coffee), HttpStatus.CREATED)
+				: new ResponseEntity<>(coffee, HttpStatus.OK);
+	}
+
+	@DeleteMapping("/{id}")
+	void deleteCoffee(@PathVariable String id) {
+		coffees.removeIf(c -> c.getId().equals(id));
+	}
+}
